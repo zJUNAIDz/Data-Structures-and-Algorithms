1a4217bd37bd8ffdfdc4124e639714031d6cde18
Junaid Shaikh <junaidshaikh3967@gmail.com>
2024-05-22 20:11:17 +0530

    add LC 121

---
+++ Array/buy-sell-stock.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <climits>
+#include <vector>
+using namespace std;
+
+int bestProfit(vector<int> arr)
+{
+
+}
+
+int main()
+{
+
+  vector<int> arr = {7, 1, 5, 3, 6, 4};
+  cout << bestProfit(arr);
+  return 0;
+}
