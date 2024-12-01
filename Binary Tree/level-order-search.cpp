125871bc9792398fe23fb77345c8acf30cca62a9
zJUNAIDz <junaidshaikh3967@gmail.com>
2024-12-01 19:57:57 +0530

    add level order search for BFS

---
+++ Binary Tree/level-order-search.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+struct TreeNode
+{
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+vector<vector<int>> levelOrder(TreeNode *root)
+{
+  vector<vector<int>> ans;
+  if (root == NULL)
+    return ans;
+  queue<TreeNode *> q;
+  q.push(root);
+  while (!q.empty())
+  {
+    int size = q.size();
+    vector<int> level;
+    for (int i = 0; i < size; i++)
+    {
+      TreeNode *node = q.front();
+      q.pop();
+      if (node->left)
+        q.push(node->left);
+      if (node->right)
+        q.push(node->right);
+      level.push_back(node->val);
+    }
+    ans.push_back(level);
+  }
+  return ans;
+}
+
+int main()
+{
+  TreeNode *root = new TreeNode(10);
+  root->left = new TreeNode(20);
+  root->right = new TreeNode(30);
+  root->right->left = new TreeNode(40);
+
+  vector<vector<int>> bfs = levelOrder(root);
+  int rowSize = bfs.size();
+  for (int i = 0; i < rowSize; i++)
+  {
+    for (int j = 0; j < bfs[i].size(); j++)
+    {
+      cout << bfs[i][j] << " ";
+    }
+    cout << endl;
+  }
+  return 0;
+}
