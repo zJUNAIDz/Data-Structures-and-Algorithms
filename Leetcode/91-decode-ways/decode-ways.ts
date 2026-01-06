ccaac99e173adef1d0df737eaa5f9bc5bd63eb20
Junaid Shaikh <junaidshaikh3967@gmail.com>
2026-01-06 17:38:53 +0530

    Time: 3 ms (14.83%) | Memory: 56.7 MB (20.53%) - LeetSync

--- Leetcode/91-decode-ways/decode-ways.ts
+++ Leetcode/91-decode-ways/decode-ways.ts
@@ -1,19 +1,20 @@
-// similar to fibonacci and coin change and word break, optimization from decision tree (2^n) -> memoization -> tabulation 
+// similar to fibonacci and coin change and word break, optimization from decision tree (2^n) -> memoization -> tabulation -> space optimization (using only teo previous values)
 function numDecodings(s: string): number {
     if (s.length === 0 || s[0] === "0") return 0;
-    const dp: number[] = Array(s.length + 1).fill(0);
-    
-    dp[0] = dp[1] = 1;
+    let prev1 = 1, prev2 = 1; 
     for (let i = 2; i <= s.length; i++) {
-        const oneDigit: number = parseInt(s[i - 1]); // previous one digit
-        const twoDigits: number = parseInt(s[i - 2] + s[i - 1]); // previous two digits
+        let curr: number = 0;
+        const oneDigit: number = parseInt(s[i - 1]);
+        const twoDigits: number = parseInt(s[i - 2] + s[i - 1]);
 
-        if (oneDigit >= 1 && oneDigit <= 9) { 
-            dp[i] += dp[i - 1];
+        if (oneDigit >= 1 && oneDigit <= 9) {
+            curr += prev1;
         }
         if (twoDigits >= 10 && twoDigits <= 26) {
-            dp[i] += dp[i - 2];
+            curr += prev2;
         }
+        prev2 = prev1;
+        prev1 = curr;
     }
-    return dp[s.length]
+    return prev1;
 };
